751c8cb785454ef7c76f5adb3111577fc1bfadfd
“salihkaraca” <karacasalih@yahoo.com>
2020-05-05 18:51:17 +0200

    add login.java

---
+++ src/tests/login.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class login {
+
+    public static void main(String[] args) {
+
+        for(int i=0; i<10; i++){
+
+            System.out.println("int = " + i);
+
+        }
+    }
+}
