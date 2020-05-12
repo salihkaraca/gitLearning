d9c3e0649d6be8952bcf48a6a957718b1ba73d5e
“salihkaraca” <karacasalih@yahoo.com>
2020-05-12 16:46:43 +0200

    add CoserInTheHouse.java

---
+++ src/tests/CoserInTheHouse.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class CoserInTheHouse {
+
+    public static void Main(String[] args){
+
+
+        System.out.println("create new class and push the git exercise");
+    }
+}
